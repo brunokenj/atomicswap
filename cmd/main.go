d39fcc4bff376a40b6566ddd78d17fead8382895
Bruno Kenj <bruno.kenj@gmail.com>
2017-10-01 19:47:36 -0300

    adding eth

---
+++ cmd/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"encoding/hex"
+	"strings"
+
+	simplejson "github.com/bitly/go-simplejson"
+	"github.com/makevoid/web3_go/web3"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	account := coinbase()
+	fmt.Println("account:", account)
+
+	accs := accounts()
+	fmt.Println("accounts:", accs)
+
+	bal := getBalance(account)
+	fmt.Println("balance of account:", account, "=", bal)
+
+	contract := "contract test { function multiply(uint a) returns(uint d) {   return a * 7;   } }"
+	resp_c := compile(contract)
+	fmt.Println("compiled contract infos (abi):")
+	info_c := getAbi(resp_c)
+	pp(info_c)
+
+	sha := crypto.Keccak256([]byte("data()"))
+	fmt.Println("sha3('data()'):", sha)
+	shaHex := hex.EncodeToString(sha[:])
+	// fmt.Println("sha3('foo'):", string(sha[:32]))
+	fmt.Println("sha3('data()'):", shaHex)
+	fmt.Println("method data() signature:", shaHex[0:8])
+
+	data := "0x" + shaHex[0:8]
+	to := "0x40ab5d8120b5ac6585302461a4a0320d023a16bf"
+
+	resp := call(data, to)
+	fmt.Println("call method(), resp:", resp)
+	str := resp.(string)
+	str = strings.TrimPrefix(str, "0x")
+	str = string(str)
+	value, err := hex.DecodeString(str)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("value (as bytes):", value)
+	fmt.Println("value (as string):", string(value))
+
+	// TODO:
+	// eth_sendTransaction - https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_sendtransaction
+	// eth_sign
+
+}
+
+func compile(contract string) *simplejson.Json {
+	//  TODO: compile with solc
+	res := web3.Call("eth_compileSolidity", `["`+contract+`"]`).Get("result")
+	return res
+}
+
+func getAbi(compiledResp *simplejson.Json) interface{} {
+	return compiledResp.Get("test").Get("info").Get("abiDefinition").MustArray()
+}
+
+func call(data string, to string) interface{} {
+	callArgs := `[{ "to": "` + to + `", "data": "` + data + `" }]`
+	// fmt.Println(callArgs)
+	res := web3.Call("eth_call", callArgs)
+	errStr := res.Get("error").MustString()
+	if errStr != "" {
+		fmt.Println("Error: " + errStr)
+		pp(res)
+	}
+	resp := res.Get("result").MustString()
+	return resp
+}
+
+func coinbase() string {
+	res := web3.Call("eth_coinbase").Get("result").MustString()
+	return res
+}
+
+func accounts() []interface{} {
+	res := web3.Call("eth_accounts").Get("result").MustArray()
+	return res
+}
+
+func getBalance(address string) string {
+	pp(web3.Call("eth_getBalance", "[\""+address+"\"]").Get("result"))
+	res := web3.Call("eth_getBalance", "[\""+address+"\"]").Get("result").MustString()
+	return res
+}
+
+// pretty print
+
+func pp(data interface{}) {
+	js, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Print(string(js))
+	println()
+}
---
+++ cmd/web3.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	"github.com/bitly/go-simplejson"
+	"github.com/parnurzeal/gorequest"
+	// "fmt"
+)
+
+const (
+	apiUrl string = "http://localhost:8545" // 8545 (default port)
+)
+
+func Call(args ...string) *simplejson.Json {
+	method := args[0]
+	params := "[]"
+	if len(args) > 1 {
+		params = args[1]
+	}
+
+	postBody := `{"jsonrpc":"2.0","method":"` + method + `","params":` + params + `}`
+	// fmt.Println("postBody: " + postBody)
+
+	_, body, errs := gorequest.New().Post(apiUrl).
+		Send(postBody).
+		End()
+
+	if errs != nil {
+		panic(errs)
+	}
+
+	js, err := simplejson.NewJson([]byte(body))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return js
+}
